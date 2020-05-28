fbbbc77e37fa494d415b03f47cb3f2c83f0f7568
Sloane Perrault <sloane.perrault@gmail.com>
2020-05-28 10:01:52 -0400

    feat(query): implement query with codec
    `<T>query(client, codec, ...args)` returns a Promise<Either<Errors,
    Array<T>>.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import * as E from 'fp-ts/lib/Either';
+import * as t from 'io-ts';
+import { failure } from 'io-ts/lib/PathReporter';
+
+const encodeErrors = E.mapLeft(failure);
+
+type Query = <T>(
+  client: any,
+  codec: t.Type<T, T, unknown>,
+  ...queryArgs: any[]
+) => Promise<E.Either<Array<string>, Array<T>>>;
+
+export const query: Query = async (client, codec, ...queryArgs) => {
+  const result: any = await client.query(...queryArgs);
+  return encodeErrors(t.array(codec).decode(result.rows));
 };
